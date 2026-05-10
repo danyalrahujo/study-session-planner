8e608d4de51fc0400cecffc0c351e15914ccddbb
danyalrahujo <danyalrahujo@192.168.7.107>
2026-05-10 22:11:21 +0200

    Add passing test for StudySessionController

--- src/main/java/com/example/studyplanner/controller/StudySessionController.java
+++ src/main/java/com/example/studyplanner/controller/StudySessionController.java
@@ -1,9 +1,17 @@
 package com.example.studyplanner.controller;
 
+import com.example.studyplanner.model.StudySession;
 import com.example.studyplanner.repository.StudySessionRepository;
 
 public class StudySessionController {
 
+	private StudySessionRepository studySessionRepository;
+
 	public StudySessionController(StudySessionRepository studySessionRepository) {
+		this.studySessionRepository = studySessionRepository;
+	}
+
+	public void addStudySession(StudySession studySession) {
+		studySessionRepository.save(studySession);
 	}
 }
