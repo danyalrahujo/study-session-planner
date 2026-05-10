e4111519378ab114254fdb51c2295c3215046e8c
danyalrahujo <danyalrahujo@192.168.7.107>
2026-05-10 22:39:12 +0200

    Add passing test for TagController

--- src/main/java/com/example/studyplanner/controller/TagController.java
+++ src/main/java/com/example/studyplanner/controller/TagController.java
@@ -1,9 +1,17 @@
 package com.example.studyplanner.controller;
 
+import com.example.studyplanner.model.Tag;
 import com.example.studyplanner.repository.TagRepository;
 
 public class TagController {
 
+	private TagRepository tagRepository;
+
 	public TagController(TagRepository tagRepository) {
+		this.tagRepository = tagRepository;
+	}
+
+	public void addTag(Tag tag) {
+		tagRepository.save(tag);
 	}
 }
