689434574955034b4f0368fc891112f63f0ab320
danyalrahujo <danyalrahujo@192.168.7.107>
2026-05-14 14:19:36 +0200

    Implement deleteTag and updateTag in TagController

--- src/main/java/com/example/studyplanner/controller/TagController.java
+++ src/main/java/com/example/studyplanner/controller/TagController.java
@@ -19,4 +19,12 @@ public class TagController {
 	public List<Tag> getAllTags() {
 		return tagRepository.findAll();
 	}
+
+	public void deleteTag(String id) {
+		tagRepository.delete(id);
+	}
+
+	public void updateTag(Tag tag) {
+		tagRepository.update(tag);
+	}
 }
