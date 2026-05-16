2e15608cd4e039609396fc1a8f0eaa2642339814
danyalrahujo <danyalrahujo@192.168.7.107>
2026-05-16 22:53:03 +0200

    Add failing test for findById in InMemoryStudySessionRepository

--- src/test/java/com/example/studyplanner/repository/InMemoryStudySessionRepositoryTest.java
+++ src/test/java/com/example/studyplanner/repository/InMemoryStudySessionRepositoryTest.java
@@ -30,4 +30,16 @@ public class InMemoryStudySessionRepositoryTest {
 
 		assertEquals(1, result.size());
 	}
+
+	@Test
+	public void testFindById() {
+
+		StudySession studySession = new StudySession("1", "Study TDD", false, "2025-05-10", null);
+
+		inMemoryStudySessionRepository.save(studySession);
+
+		StudySession result = inMemoryStudySessionRepository.findById("1");
+
+		assertEquals("Study TDD", result.getDescription());
+	}
 }
