df264f38dbf3e600517f91f5e79492f82b9e1ed8
danyalrahujo <danyalrahujo@192.168.7.107>
2026-05-16 22:45:52 +0200

    failing test for InMemoryStudySessionRepository save

---
+++ src/test/java/com/example/studyplanner/repository/InMemoryStudySessionRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.studyplanner.repository;
+
+import com.example.studyplanner.model.StudySession;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class InMemoryStudySessionRepositoryTest {
+
+	private InMemoryStudySessionRepository inMemoryStudySessionRepository;
+
+	@Before
+	public void setUp() {
+
+		inMemoryStudySessionRepository = new InMemoryStudySessionRepository();
+	}
+
+	@Test
+	public void testSave() {
+
+		StudySession studySession = new StudySession("1", "Study TDD", false, "2025-05-10", null);
+
+		inMemoryStudySessionRepository.save(studySession);
+
+		List<StudySession> result = inMemoryStudySessionRepository.findAll();
+
+		assertEquals(1, result.size());
+	}
+}
