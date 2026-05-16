bf5e0448df15b4d47f51ab57e2093840c923b63e
danyalrahujo <danyalrahujo@192.168.7.107>
2026-05-16 23:23:24 +0200

    Implement save and findAll in InMemoryTagRepository

--- src/main/java/com/example/studyplanner/repository/InMemoryTagRepository.java
+++ src/main/java/com/example/studyplanner/repository/InMemoryTagRepository.java
@@ -1,19 +1,22 @@
 package com.example.studyplanner.repository;
 
 import com.example.studyplanner.model.Tag;
+import java.util.ArrayList;
 
 import java.util.List;
 
 public class InMemoryTagRepository implements TagRepository {
 
+	private List<Tag> tags = new ArrayList<>();
+
 	@Override
 	public void save(Tag tag) {
-
+		tags.add(tag);
 	}
 
 	@Override
 	public List<Tag> findAll() {
-		return null;
+		return tags;
 	}
 
 	@Override
