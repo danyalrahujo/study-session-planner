03906c7f9bd0378ad16aca8142ec6382f5237d46
danyalrahujo <danyalrahujo@192.168.7.107>
2026-05-16 23:11:07 +0200

    Implement delete update and findByTag in InMemoryStudySessionRepository

--- src/main/java/com/example/studyplanner/repository/InMemoryStudySessionRepository.java
+++ src/main/java/com/example/studyplanner/repository/InMemoryStudySessionRepository.java
@@ -2,6 +2,7 @@ package com.example.studyplanner.repository;
 
 import java.util.List;
 import java.util.ArrayList;
+import com.example.studyplanner.model.Tag;
 
 import com.example.studyplanner.model.StudySession;
 
@@ -33,21 +34,49 @@ public class InMemoryStudySessionRepository implements StudySessionRepository {
 	}
 
 	@Override
-	public void update(StudySession studySession) {
-		// TODO Auto-generated method stub
+	public void update(StudySession updatedStudySession) {
 
+		for (int i = 0; i < studySessions.size(); i++) {
+
+			StudySession currentStudySession = studySessions.get(i);
+
+			if (currentStudySession.getId().equals(updatedStudySession.getId())) {
+
+				studySessions.set(i, updatedStudySession);
+			}
+		}
 	}
 
 	@Override
 	public void delete(String id) {
-		// TODO Auto-generated method stub
 
+		studySessions.removeIf(studySession -> studySession.getId().equals(id));
 	}
 
 	@Override
 	public List<StudySession> findByTag(String tagId) {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<StudySession> filteredStudySessions = new ArrayList<>();
+
+		for (StudySession studySession : studySessions) {
+
+			List<Tag> tags = studySession.getTags();
+
+			if (tags != null) {
+
+				for (Tag tag : tags) {
+
+					if (tag.getId().equals(tagId)) {
+
+						filteredStudySessions.add(studySession);
+
+						break;
+					}
+				}
+			}
+		}
+
+		return filteredStudySessions;
 	}
 
 }
