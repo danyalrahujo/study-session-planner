c5da082f66daf860466ccffe92b9d44eda3c3f80
danyalrahujo <danyalrahujo@192.168.7.107>
2026-05-18 02:56:53 +0200

    Pass non-empty Mongo repository test

--- src/main/java/com/example/studyplanner/repository/mongo/MongoStudySessionRepository.java
+++ src/main/java/com/example/studyplanner/repository/mongo/MongoStudySessionRepository.java
@@ -7,20 +7,46 @@ import com.example.studyplanner.model.StudySession;
 import com.example.studyplanner.repository.StudySessionRepository;
 import com.mongodb.MongoClient;
 
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
+
 public class MongoStudySessionRepository implements StudySessionRepository {
+	public static final String STUDY_SESSION_COLLECTION_NAME = "studySession";
+
+	public static final String STUDY_PLANNER_DB_NAME = "studyPlanner";
+
+	private MongoCollection<Document> studySessionCollection;
 
 	public MongoStudySessionRepository(MongoClient client) {
 
+		studySessionCollection = client.getDatabase(STUDY_PLANNER_DB_NAME).getCollection(STUDY_SESSION_COLLECTION_NAME);
 	}
 
 	@Override
 	public void save(StudySession studySession) {
 
+		Document document = new Document();
+
+		document.append("id", studySession.getId());
+		document.append("description", studySession.getDescription());
+
+		studySessionCollection.insertOne(document);
 	}
 
 	@Override
 	public List<StudySession> findAll() {
-		return new ArrayList<>();
+		List<StudySession> studySessions = new ArrayList<>();
+
+		for (Document document : studySessionCollection.find()) {
+
+			StudySession studySession = new StudySession(document.getString("id"), document.getString("description"),
+					false, null, null);
+
+			studySessions.add(studySession);
+		}
+
+		return studySessions;
 	}
 
 	@Override
