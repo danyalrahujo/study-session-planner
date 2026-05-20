55ed037c8e3d485e82ce63e38949babf8d5d33c6
danyalrahujo <danyalrahujo@192.168.7.161>
2026-05-20 15:57:19 +0200

    Configure Fail-safe plugin for integration tests

--- src/it/java/com/example/studyplanner/repository/mongo/MongoStudySessionRepositoryIT.java
+++ src/it/java/com/example/studyplanner/repository/mongo/MongoStudySessionRepositoryIT.java
@@ -2,18 +2,62 @@ package com.example.studyplanner.repository.mongo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.bson.Document;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+
 import org.testcontainers.containers.GenericContainer;
+
+import com.example.studyplanner.model.StudySession;
+
+import static com.example.studyplanner.repository.mongo.MongoStudySessionRepository.STUDY_PLANNER_DB_NAME;
+import static com.example.studyplanner.repository.mongo.MongoStudySessionRepository.STUDY_SESSION_COLLECTION_NAME;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class MongoStudySessionRepositoryIT {
 
+	@SuppressWarnings("rawtypes")
+
 	@ClassRule
-	public static GenericContainer<?> mongo = new GenericContainer<>("mongo:4.4.3").withExposedPorts(27017);
+	public static final GenericContainer mongo = new GenericContainer("mongo:4.4.2").withExposedPorts(27017);
+
+	private MongoClient client;
+
+	private MongoStudySessionRepository studySessionRepository;
+
+	private MongoCollection<Document> studySessionCollection;
+
+	@Before
+	public void setup() {
+
+		client = new MongoClient(new ServerAddress(mongo.getHost(), mongo.getFirstMappedPort()));
+
+		studySessionRepository = new MongoStudySessionRepository(client);
+
+		MongoDatabase database = client.getDatabase(STUDY_PLANNER_DB_NAME);
+
+		database.drop();
+
+		studySessionCollection = database.getCollection(STUDY_SESSION_COLLECTION_NAME);
+	}
+
+	@After
+	public void tearDown() {
+
+		client.close();
+	}
 
 	@Test
-	public void testContainerIsRunning() {
+	public void testFindAll() {
 
-		assertThat(mongo.isRunning()).isTrue();
+		assertThat(studySessionRepository.findAll()).isEmpty();
 	}
 }
