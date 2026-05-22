256085b9a60c7a8be8bf49532f3e6d9e506252c4
danyalrahujo <danyalrahujo@192.168.7.161>
2026-05-22 12:53:05 +0200

    Integration Tests with Maven and Docker

--- src/it/java/com/example/studyplanner/repository/mongo/MongoStudySessionRepositoryIT.java
+++ src/it/java/com/example/studyplanner/repository/mongo/MongoStudySessionRepositoryIT.java
@@ -1,33 +1,24 @@
 package com.example.studyplanner.repository.mongo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.bson.Document;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Test;
-
-import org.testcontainers.containers.GenericContainer;
-
-import com.example.studyplanner.model.StudySession;
-
 import static com.example.studyplanner.repository.mongo.MongoStudySessionRepository.STUDY_PLANNER_DB_NAME;
 import static com.example.studyplanner.repository.mongo.MongoStudySessionRepository.STUDY_SESSION_COLLECTION_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.studyplanner.model.StudySession;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
-
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoStudySessionRepositoryIT {
-
-	@SuppressWarnings("rawtypes")
-
-	@ClassRule
-	public static final GenericContainer mongo = new GenericContainer("mongo:4.4.2").withExposedPorts(27017);
 
 	private MongoClient client;
 
@@ -38,8 +29,7 @@ public class MongoStudySessionRepositoryIT {
 	@Before
 	public void setup() {
 
-		client = new MongoClient(new ServerAddress(mongo.getHost(), mongo.getFirstMappedPort()));
-
+		client = new MongoClient(new ServerAddress("localhost", 27017));
 		studySessionRepository = new MongoStudySessionRepository(client);
 
 		MongoDatabase database = client.getDatabase(STUDY_PLANNER_DB_NAME);
@@ -58,6 +48,80 @@ public class MongoStudySessionRepositoryIT {
 	@Test
 	public void testFindAll() {
 
-		assertThat(studySessionRepository.findAll()).isEmpty();
+		addTestStudySessionToDatabase("1", "Study TDD");
+
+		addTestStudySessionToDatabase("2", "Study Mockito");
+
+		assertThat(studySessionRepository.findAll()).containsExactly(
+				new StudySession("1", "Study TDD", false, null, null),
+				new StudySession("2", "Study Mockito", false, null, null));
 	}
+
+	@Test
+	public void testFindById() {
+
+		addTestStudySessionToDatabase("1", "Study TDD");
+
+		addTestStudySessionToDatabase("2", "Study Mockito");
+
+		assertThat(studySessionRepository.findById("2"))
+				.isEqualTo(new StudySession("2", "Study Mockito", false, null, null));
+	}
+
+	@Test
+	public void testSave() {
+
+		StudySession studySession = new StudySession("1", "Study TDD", false, null, null);
+
+		studySessionRepository.save(studySession);
+
+		assertThat(readAllStudySessionsFromDatabase()).containsExactly(studySession);
+	}
+
+	@Test
+	public void testDelete() {
+
+		addTestStudySessionToDatabase("1", "Study TDD");
+
+		studySessionRepository.delete("1");
+
+		assertThat(readAllStudySessionsFromDatabase()).isEmpty();
+	}
+
+	@Test
+	public void testUpdate() {
+
+		StudySession studySession = new StudySession("1", "Study TDD", false, null, null);
+
+		studySessionRepository.save(studySession);
+
+		assertThat(readAllStudySessionsFromDatabase()).containsExactly(studySession);
+
+		StudySession updatedStudySession = new StudySession("1", "Study Mockito", false, null, null);
+
+		studySessionRepository.update(updatedStudySession);
+
+		assertThat(readAllStudySessionsFromDatabase()).containsExactly(updatedStudySession);
+	}
+
+	private void addTestStudySessionToDatabase(String id, String description) {
+
+		studySessionCollection.insertOne(new Document().append("id", id).append("description", description));
+	}
+
+	private List<StudySession> readAllStudySessionsFromDatabase() {
+
+		List<StudySession> studySessions = new ArrayList<>();
+
+		for (Document document : studySessionCollection.find()) {
+
+			StudySession studySession = new StudySession(document.getString("id"), document.getString("description"),
+					false, null, null);
+
+			studySessions.add(studySession);
+		}
+
+		return studySessions;
+	}
+
 }
