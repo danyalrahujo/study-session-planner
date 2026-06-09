d9a2b9ac6abbdbef2a73e0161ba6688d97931bad
danyalrahujo <danyalrahujo@192.168.7.107>
2026-06-09 22:43:01 +0200

    Add failing test for delete tag button selection state

--- src/test/java/com/example/studyplanner/view/swing/TagSwingViewTest.java
+++ src/test/java/com/example/studyplanner/view/swing/TagSwingViewTest.java
@@ -7,17 +7,19 @@ import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Test;
+import com.example.studyplanner.model.Tag;
 
 public class TagSwingViewTest extends AssertJSwingJUnitTestCase {
 
 	private FrameFixture window;
 
+	private TagSwingView tagSwingView;
+
 	@Override
 	protected void onSetUp() {
+		tagSwingView = GuiActionRunner.execute(() -> new TagSwingView());
 
-		TagSwingView view = GuiActionRunner.execute(() -> new TagSwingView());
-
-		window = new FrameFixture(robot(), view);
+		window = new FrameFixture(robot(), tagSwingView);
 
 		window.show();
 	}
@@ -45,5 +47,35 @@ public class TagSwingViewTest extends AssertJSwingJUnitTestCase {
 		window.textBox("tagNameTextBox").enterText("Java");
 
 		window.button(JButtonMatcher.withText("Add Tag")).requireEnabled();
+	}
+
+	@Test
+	public void testWhenTagNameIsBlankThenAddTagButtonShouldBeDisabled() {
+
+		window.textBox("tagNameTextBox").enterText("Java");
+
+		window.button(JButtonMatcher.withText("Add Tag")).requireEnabled();
+
+		window.textBox("tagNameTextBox").setText("");
+
+		window.textBox("tagNameTextBox").enterText(" ");
+
+		window.button(JButtonMatcher.withText("Add Tag")).requireDisabled();
+	}
+
+	@Test
+	public void testDeleteTagButtonShouldBeEnabledOnlyWhenATagIsSelected() {
+
+		Tag tag = new Tag("1", "Java");
+
+		GuiActionRunner.execute(() -> tagSwingView.getListTagsModel().addElement(tag));
+
+		window.list("tagList").selectItem(0);
+
+		window.button(JButtonMatcher.withText("Delete Selected")).requireEnabled();
+
+		window.list("tagList").clearSelection();
+
+		window.button(JButtonMatcher.withText("Delete Selected")).requireDisabled();
 	}
 }
