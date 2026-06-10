9ddca9503bd274960871a985bff65f2f8eee34e9
danyalrahujo <danyalrahujo@192.168.7.107>
2026-06-10 20:44:47 +0200

    add basic TagSwingView integration test

---
+++ src/it/java/com/example/studyplanner/view/swing/TagSwingViewIT.java
@@ -0,0 +1,37 @@
+package com.example.studyplanner.view.swing;
+
+import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
+
+public class TagSwingViewIT extends AssertJSwingJUnitTestCase {
+
+	private FrameFixture window;
+
+	@Override
+	protected void onSetUp() {
+
+		TagSwingView view = GuiActionRunner.execute(() -> new TagSwingView());
+
+		window = new FrameFixture(robot(), view);
+
+		window.show();
+	}
+
+	@Override
+	protected void onTearDown() {
+
+		if (window != null) {
+			window.cleanUp();
+		}
+	}
+
+	@Test
+	@GUITest
+	public void testWindowIsVisible() {
+
+		window.requireVisible();
+	}
+}
